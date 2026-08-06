455185590141288244d1fb6cdfa114790a2804f7
Eduardo <eduardo_reolon@hotmail.com>
2026-08-06 17:01:02 -0300

    ajustes

--- public/widget.js
+++ public/widget.js
@@ -182,7 +182,6 @@
             fa.rel = 'stylesheet';
             fa.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css';
             
-            // TRUQUE DE PERFORMANCE: Carrega de forma assíncrona (não bloqueia a tela)
             fa.media = 'print';
             fa.onload = function() { this.media = 'all'; };
             
